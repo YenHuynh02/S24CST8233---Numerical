b8bd0b3865fa897fa9745f14635e21d5380cc08c
Yen Huynh <peterhuynh.contact@gmail.com>
2024-07-09 19:22:49 -0400

    Update Lab4.R

--- Lab4.R
+++ Lab4.R
@@ -10,7 +10,7 @@ maclaurin_ln <- function(x, n_terms) {
 
 # Function for absolute and relative errors
 compute_errors <- function(x, n_terms) {
-  #log() is a natural logarithm(ln), log10() is a base 10 logarithm
+  # log() is a natural logarithm (ln), log10() is a base 10 logarithm
   true_value <- log(1 + x)
   absolute <- numeric(n_terms)
   relative <- numeric(n_terms)
@@ -27,12 +27,29 @@ compute_errors <- function(x, n_terms) {
   return(data.frame(n = 1:n_terms, Absolute_Error = absolute, Relative_Error = relative))
 }
 
-# Plotting the original function and the approximation
-plot_function <- function() {
+# Plotting the original function
+plot_original_function <- function() {
   x_vals <- seq(-0.5, 0.5, by = 0.01)
   y_true <- log(1 + x_vals)
   
-  plot(x_vals, y_true, type = "l", ylab = "f(x)", xlab = "x", main = "f(6) for Maclaurin Series Approximation")
+  plot(x_vals, y_true, type = "l", ylab = "f(x)", xlab = "x",
+       main = "Original Function: ln(1 + x)")
+}
+
+# Plotting the Maclaurin series approximationA
+plot_approximation <- function() {
+  x_vals <- seq(-0.5, 0.5, by = 0.01)
+  y_approx <- sapply(x_vals, function(x) maclaurin_ln(x, 6))
+  
+  plot(x_vals, y_approx, type="l", ylab = "f(x)", xlab = "x",
+       main = "f(6) - Maclaurin Approximation")
+}
+
+# Plot both the original function and the approximation side by side
+plot_side_by_side <- function() {
+  par(mfrow = c(1, 2))  # Set the plotting area to have 1 row and 2 columns
+  plot_original_function()
+  plot_approximation()
 }
 
 # Main script
@@ -43,9 +60,7 @@ main <- function() {
   # Compute errors
   errors <- compute_errors(x, n_terms)
   print(errors)
-  
-  # Plot the functions
-  plot_function()
+  plot_side_by_side()
 }
 
 # Run the main script
